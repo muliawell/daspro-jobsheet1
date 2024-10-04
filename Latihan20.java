2270f82aef7e835f86180f585e91ed1382ee3a58
muliawell <raragombel01@gmail.com>
2024-10-04 23:37:53 +0700

    Add files via upload

---
+++ Latihan20.java
@@ -0,0 +1,6 @@
+public class Latihan20 {
+    public static void main(String[] args) {
+        System.out.println("Nama saya Mulia Rahma Delvia");
+        System.out.println("Saya adalah mahasiswa baru Jurusan Teknologi Informasi-POLINEMA");
+    }
+    }
---
+++ MyFirstJava20.java
@@ -0,0 +1,15 @@
+public class MyFirstJava20 {
+    public static void main(String[] args) {
+        System.out.println("-----------------------------------------------");
+        System.out.println("-- Data Mahasiswa Jurusan Teknologi POLINEMA --");
+        System.out.println("-----------------------------------------------");
+        System.out.println("Nama    : Mulia Rahma Delvia");
+        System.out.println("NIM     : 244107060028");
+        System.out.println("Kelas   : 1F");
+        System.out.println("No.Absen: 10");
+        System.out.println("Prodi   : D-IV-Sistem Informasi Bisnis");
+        System.out.println("Alamat  : Jl. Perbakin ");
+        System.out.println("No HP   : 081991112378");
+        System.out.println("Hobi    : Bermain Badminton dan Game Online");
+    }
+    }
